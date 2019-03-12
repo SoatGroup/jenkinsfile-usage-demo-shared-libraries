21557ce5a3c30c7edd8a5773a4f70ba6ff78a65c
Patrick Allain <patrick.allain@soat.fr>
2019-03-12 20:21:16 +0100

    WIP

--- vars/springBoot.groovy
+++ vars/springBoot.groovy
@@ -40,7 +40,10 @@ def call(Map config) {
 
             stage('PREPARE') {
                 steps {
-                   sh("mkdir ${HOME}/.m2 && chown -R 1000:1000 ${HOME}/.m2")
+                   sh("""
+                   mkdir ${HOME}/.m2 || true
+                   chown -R 1000:1000 ${HOME}/.m2
+                   """)
                 }
             }
 
